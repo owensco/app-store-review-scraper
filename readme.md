70da1a90d13f3ce43c1111bfb8ab1c61f1fe3d6e
oweskeez <oscott96@gmail.com>
2020-04-23 13:30:26 -0400

    initial commit

---
+++ readme.md
@@ -0,0 +1,24 @@
+Requirements:
+- node.js (https://nodejs.org/en/)
+- app-store-scraper (https://www.npmjs.com/package/app-store-scraper)
+- csv-writer (https://www.npmjs.com/package/csv-writer)
+
+Known issues:
+1. Special characters may break as some apps do not encode in unicode by default.
+- Microsoft Excel:
+Open CSV > Data > From Text/CSV > find csv > file origin = Unicode (UTF-8) > Load
+- Google Sheets:
+Create new sheet > File > Import > upload csv > Seperator type = Comma > Import
+
+2. Script occasionally throws:
+`
+{ Error: getaddrinfo ENOTFOUND itunes.apple.com itunes.apple.com:443
+    at GetAddrInfoReqWrap.onlookup [as oncomplete] (dns.js:56:26)   
+  errno: 'ENOTFOUND',
+  code: 'ENOTFOUND',
+  syscall: 'getaddrinfo',
+  hostname: 'itunes.apple.com',
+  host: 'itunes.apple.com',
+  port: 443 }
+`
+Re-run the script until successful, may take a few tries.
---
+++ scraper.js
@@ -0,0 +1,59 @@
+//your app id
+var app_id = '';
+//timestamp for filename
+var time = process.hrtime();
+var timestamp = Math.round( time[ 0 ] * 1e3 + time[ 1 ] / 1e6 );
+//init csv writer
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const csvWriter = createCsvWriter({
+  path: './reviews/app_store_reviews_' + timestamp + '.csv',
+  header: [
+    {id: 'userName', title: 'User'},
+    {id: 'userUrl', title: 'User URL'},
+    {id: 'version', title: 'App Version'},
+    {id: 'score', title: 'Stars'},
+    {id: 'title', title: 'Review Title'},
+    {id: 'text', title: 'Review Text'},
+    {id: 'url', title: 'Review URL'},
+  ]
+});
+//init store-scraper
+var store = require('app-store-scraper');
+//import countries list
+var countries = require('./countries.json')
+//start scraping page 1
+function reviewScraper(page = 1, country_counter = 0){
+    store.reviews({
+    id: app_id,
+    country: countries[country_counter],
+    page: page
+    })
+.then(function(data){
+    //if reviews are returned
+    if(data != ''){
+        //write reviews to csv
+        csvWriter
+            .writeRecords(data)
+        //wipe data from previous page
+        delete data;
+        //scrape next page
+        page++;
+        reviewScraper(page, country_counter);
+    }
+    //if no more reviews for that country
+    else if(data == '' && country_counter != (countries.length - 1)){
+        //wipe data from previous run
+        delete data;
+        //scrape next country
+        country_counter++;
+        reviewScraper(page = 1, country_counter);
+    }
+    //scrape complete!
+    else{
+        console.log('scrape successful')
+        return false;
+    }
+  })
+  .catch(console.log);
+}
+reviewScraper();
